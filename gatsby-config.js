fa87203162fc78d7f837f6168bd90d1de3122283
Youssouf El Azizi <youssoufelazizi@gmail.com>
2019-01-02 00:31:38 +0100

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,49 +1,55 @@
-const config = require('./config/SiteConfig');
+const config = require("./config/SiteConfig");
 
-const pathPrefix = config.pathPrefix === '/' ? '' : config.pathPrefix;
+const pathPrefix = config.pathPrefix === "/" ? "" : config.pathPrefix;
 
 module.exports = {
   pathPrefix: config.pathPrefix,
   siteMetadata: {
-    siteUrl: config.siteUrl + pathPrefix,
+    siteUrl: config.siteUrl + pathPrefix
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-styled-components',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-styled-components",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'post',
-        path: `${__dirname}/blog`,
-      },
+        name: "post",
+        path: `${__dirname}/blog`
+      }
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-external-links',
+            resolve: "gatsby-remark-external-links",
             options: {
-              target: '_blank',
-              rel: 'nofollow noopener noreferrer',
-            },
+              target: "_blank",
+              rel: "nofollow noopener noreferrer"
+            }
           },
-          'gatsby-remark-prismjs',
-          'gatsby-remark-autolink-headers',
-        ],
-      },
+          "gatsby-remark-prismjs",
+          "gatsby-remark-autolink-headers"
+        ]
+      }
     },
     {
-      resolve: 'gatsby-plugin-typography',
+      resolve: "gatsby-plugin-typography",
       options: {
-        pathToConfigModule: 'src/utils/typography.js',
-      },
+        pathToConfigModule: "src/utils/typography.js"
+      }
     },
-    'gatsby-plugin-catch-links',
-    'gatsby-plugin-sitemap',
-    'gatsby-plugin-lodash',
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-127901499-2"
+      }
+    },
+    "gatsby-plugin-catch-links",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-lodash",
+    {
+      resolve: "gatsby-plugin-manifest",
       options: {
         name: config.siteTitle,
         short_name: config.siteTitleAlt,
@@ -51,11 +57,10 @@ module.exports = {
         start_url: config.pathPrefix,
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
-        display: 'standalone',
-        icon: config.favicon,
-      },
+        display: "standalone",
+        icon: config.favicon
+      }
     },
-    'gatsby-plugin-offline',
-    'gatsby-plugin-netlify',
-  ],
+    "gatsby-plugin-netlify"
+  ]
 };
