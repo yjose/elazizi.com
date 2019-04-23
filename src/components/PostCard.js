10d820b6f7961d35f4e0d3dda7966402e42b4dca
Youssouf El Azizi <youssoufelazizi@gmail.com>
2019-04-23 13:59:59 +0100

    make the whole Article's  box clickable.

--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -9,10 +9,6 @@ import theme from "../../config/theme";
 const PostTitle = styled.h2`
   margin-bottom: ${rhythm(0.3)};
   transition: ${theme.transition.ease};
-  :hover {
-    color: ${theme.brand.primary};
-    transition: ${theme.transition.ease};
-  }
 `;
 
 const Description = styled.p`
@@ -22,31 +18,32 @@ const Description = styled.p`
 `;
 
 export default ({ post }) => (
-  <div
-    css={css`
-      margin-bottom: 40px;
-      background: white;
-      padding: 10px 20px 10px 20px;
-      box-shadow: 0 1px 2px rgba(0, 0, 0, 0.07);
-      border-radius: 5px;
-    `}
-  >
-    <Link to={post.fields.slug} aria-label={`View ${post.frontmatter.title}`}>
-      <PostTitle>{post.frontmatter.title}</PostTitle>
-    </Link>
-
-    <Description>{post.excerpt}</Description>
+  <Link to={post.fields.slug} aria-label={`View ${post.frontmatter.title}`}>
     <div
       css={css`
-        display: flex;
-        justify-content: flex-end;
-        padding: 10px;
+        margin-bottom: 40px;
+        background: white;
+        padding: 10px 20px 10px 20px;
+        box-shadow: 0 1px 2px rgba(0, 0, 0, 0.07);
+        border-radius: 5px;
+        cursor: pointer;
+        :hover {
+          box-shadow: 0 1px 2px rgba(0, 0, 0, 0.27);
+          transition: ${theme.transition.ease};
+        }
+        color: rgba(0, 0, 0, 0.85);
       `}
     >
-      <Link to={post.fields.slug} aria-label={`View ${post.frontmatter.title}`}>
-        Read Article →
-      </Link>
+      <PostTitle>{post.frontmatter.title}</PostTitle>
+      <Description>{post.excerpt}</Description>
+      <div
+        css={css`
+          display: flex;
+          justify-content: flex-end;
+          padding: 10px;
+        `}
+      />
+      <span />
     </div>
-    <span />
-  </div>
+  </Link>
 );
