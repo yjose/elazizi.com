b8d59443d9619ece31c5b3c9e807fbce8b165c25
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2023-05-18 14:32:17 +0100

    init astor template

--- gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { LayoutThemeProvider } from "./src/components/Theming";
-
-export const wrapRootElement = ({ element }) => {
-  return <LayoutThemeProvider> {element} </LayoutThemeProvider>;
-};
