4cfc04f052d876f94d100af9e38079994c35209a
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2023-05-19 00:39:41 +0100

    add comments to blog posts

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   title: "Youssouf",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 20,
+  postPerPage: 10,
 };
 
 export const LOCALE = ["en-EN"]; // set to [] to use the environment default
